3c7bdc3121824c67ffc3639a99f020c1fa4c7cf8
Gabriel Puello <gabpuello@gmail.com>
2015-02-13 10:42:20 -0800

    add final code to method file

---
+++ lib/missing_vowels.rb
@@ -0,0 +1,23 @@
+class String
+  define_method(:missing_vowels) do
+    vowels = ["a","e","i","o","u",]
+    string = self.downcase().split("")
+
+    string.each do |letter|
+
+      if letter == "a"
+        vowels.delete("a")
+      elsif letter == "e"
+        vowels.delete("e")
+      elsif letter == "i"
+        vowels.delete("i")
+      elsif letter == "o"
+        vowels.delete("o")
+      else letter == "u"
+        vowels.delete("u")
+
+      end
+    end
+    vowels
+  end
+end
--- spec/missing_vowels_spec.rb
+++ spec/missing_vowels_spec.rb
@@ -3,5 +3,6 @@ require("missing_vowels")
 
 describe("String#missing_vowels") do
  it("returns vowels not include in a given string") do
-   expect("Epicodus".missing_vowels()).to(eq("a"))
+   expect("Epicodus".missing_vowels()).to(eq(["a"]))
+ end
 end
