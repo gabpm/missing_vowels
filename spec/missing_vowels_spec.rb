9cdb078d265dfbffb5d2235b5cd2ec38d234ac21
Gabriel Puello <gabpuello@gmail.com>
2015-02-13 10:30:19 -0800

    add code to spec file

---
+++ spec/missing_vowels_spec.rb
@@ -0,0 +1,7 @@
+require("rspec")
+require("missing_vowels")
+
+describe("String#missing_vowels") do
+ it("returns vowels not include in a given string") do
+   expect("Epicodus".missing_vowels()).to(eq("a"))
+end
